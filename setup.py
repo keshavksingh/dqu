13cfc0a0b5725442e6bf474d4b4ce6ccdd0213c1
Keshav Singh <41631964+keshavksingh@users.noreply.github.com>
2025-09-09 12:42:17 -0700

    Bump version from 0.1.0 to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dqu",
-    version="0.1.0",
+    version="0.2.3",
     description="dqu is a data quality assesement framework supporting Spark, Flink, Ray and Pandas dataframes that empowers data and machine learning data developers to perform data quality assessments. dqu provides the data quality tool you need with enterprise-grade reliability and flexibility for Flink, Microsoft Fabric, Microsoft Synapse, Microsoft Azure ML, Azure Databricks, Jupyter Notebook data and ML piplines on Ray for Python/PySpark/PyFlink environments.",
     author="Keshav Kant Singh",
     author_email="keshav_singh@hotmail.com",
@@ -12,7 +12,7 @@ setup(
         #"pandas>=1.3.0",
         #"apache-flink>=2.1.0",
         #"ray>=2.48.0",
-        "scipy>=1.16.1",
+        #"scipy>=1.16.1",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
